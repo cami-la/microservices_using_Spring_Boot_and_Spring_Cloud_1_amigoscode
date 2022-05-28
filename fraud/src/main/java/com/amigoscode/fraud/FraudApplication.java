6fec9afd33b1f1b7b48702193053f6b290d61b67
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-05-28 09:45:52 -0300

    Service Discovery and Microservices Tutorial

--- fraud/src/main/java/com/amigoscode/fraud/FraudApplication.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudApplication.java
@@ -2,10 +2,12 @@ package com.amigoscode.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import javax.swing.*;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
   public static void main(String[] args) {
     SpringApplication.run(FraudApplication.class, args);
