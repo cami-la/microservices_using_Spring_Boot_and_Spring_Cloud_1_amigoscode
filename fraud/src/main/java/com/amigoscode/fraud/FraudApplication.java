876217ef1592134eabdac131ed83c05e7f8ae499
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-05-28 07:19:17 -0300

    Microservices using Spring Boot and Spring Cloud - HTTP Communication #2

---
+++ fraud/src/main/java/com/amigoscode/fraud/FraudApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class FraudApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FraudApplication.class, args);
+  }
+}
